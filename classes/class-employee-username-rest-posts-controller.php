7bbc5521ebf1b4e6ffb6f8e8b7a1252d755d55d0
Curtiss Grymala <curt_grymala@yahoo.com>
2016-05-10 16:26:31 -0400

    Implement custom endpoint to retrieve an employee by its username

---
+++ classes/class-employee-username-rest-posts-controller.php
@@ -0,0 +1,183 @@
+<?php
+if ( ! class_exists( 'Employee_Username_REST_Posts_Controller' ) ) {
+	class Employee_Username_REST_Posts_Controller extends \WP_REST_Posts_Controller {
+		public function __construct() {
+		}
+		
+		public function get_route() {
+			return 'employee/username';
+		}
+		
+		function permissions_check() {
+			return true;
+		}
+		
+		/**
+		 * Verify that the orderby parameter is a valid key by which to order posts
+		 */
+		function valid_orderby( $key ) {
+			$keys = array( 
+				'none', 
+				'ID', 
+				'author', 
+				'title', 
+				'name', 
+				'type', 
+				'date', 
+				'modified', 
+				'parent', 
+				'rand', 
+				'comment_count', 
+				'menu_order', 
+				'post__in'
+			);
+			
+			if ( in_array( $key, $keys ) )
+				return $key;
+			
+			return 'date';
+		}
+		
+		/**
+		 * Set up a proper REST response from the data we gathered
+		 */
+		protected function create_response( $request, $args, $data ) {
+			$response    = rest_ensure_response( $data );
+			$count_query = new \WP_Query();
+			unset( $args['paged'] );
+			$query_result = $count_query->query( $args );
+			$total_posts  = $count_query->found_posts;
+			$response->header( 'X-WP-Total', (int) $total_posts );
+			$max_pages = ceil( $total_posts / $request['per_page'] );
+			$response->header( 'X-WP-TotalPages', (int) $max_pages );
+			if ( $request['page'] > 1 ) {
+				$prev_page = $request['page'] - 1;
+				if ( $prev_page > $max_pages ) {
+					$prev_page = $max_pages;
+				}
+				$prev_link = add_query_arg( 'page', $prev_page, rest_url( $this->base ) );
+				$response->link_header( 'prev', $prev_link );
+			}
+			if ( $max_pages > $request['page'] ) {
+				$next_page = $request['page'] + 1;
+				$next_link = add_query_arg( 'page', $next_page, rest_url( $this->base ) );
+				$response->link_header( 'next', $next_link );
+			}
+			return $response;
+		}
+		
+		public function get_item( $request ) {
+			$params = $request->get_params();
+			
+			if ( ! isset( $params['username'] ) || empty( $params['username'] ) )
+				return new WP_Error( 'rest_post_invalid_id', __( 'Invalid username.' ), array( 'status' => 404 ) );
+			
+			$args = array(
+				'posts_per_page' => $params['per_page'],
+				'paged'          => $params['page'],
+				'orderby'        => $params['orderby'],
+				'post_type'      => 'employee', 
+				'meta_query'     => array( array(
+					'key'     => 'wpcf-username', 
+					'value'   => $params['username'], 
+					'compare' => '=', 
+				) )
+			);
+			
+			$posts_query  = new \WP_Query();
+			$query_result = $posts_query->query( $args );
+			
+			$employee = null;
+			if ( ! empty( $query_result ) ) {
+				$employee = array_shift( $query_result );
+			}
+			
+			if ( empty( $employee ) )
+				return null;
+			
+			$data = $this->make_data( $employee, $data );
+			
+			$final =  $this->create_response( $request, $args, $data );
+			return $final;
+		}
+		
+		/**
+		 * Prepare individual items and gather appropriate meta data
+		 */
+		function make_data( $post, $data ) {
+			do_action( 'types-relationship-api-pre-make-data' );
+			
+			$image = get_post_thumbnail_id( $post->ID );
+			if ( $image ) {
+				$_image = wp_get_attachment_image_src( $image, 'large' );
+				if ( is_array( $_image ) ) {
+					$image = $_image[0];
+				}
+			}
+			
+			$data[ $post->ID ] = array(
+				'name'         => $post->post_title,
+				'link'         => get_permalink( $post->ID ),
+				'image_markup' => get_the_post_thumbnail( $post->ID, 'large' ),
+				'image_src'    => $image,
+				'excerpt'      => $post->post_excerpt,
+				'slug'         => $post->post_name, 
+				'type'         => $post->post_type, 
+			);
+			
+			$meta = apply_filters( 'types-relationship-api-post-data', array(), $post );
+			foreach ( $meta as $key => $value ) {
+				$data[ $post->ID ][$key] = get_post_meta( $post->ID, $value, true );
+			}
+			
+			do_action( 'types-relationship-api-made-data' );
+			
+			return $data;
+		}
+		
+		/**
+		 * Perform a query and prep the results to be returned
+		 * @param stdClass $request the original request object
+		 * @param array $args the query arguments
+		 * @param bool $respond whether to create a REST response from the results
+		 * @return mixed either an associative array of post objects or a REST response of post objects
+		 */
+		protected function do_query( $request, $args, $respond=true ) {
+			$posts_query  = new \WP_Query();
+			$query_result = $posts_query->query( $args );
+			$params = $request->get_params();
+			
+			if ( empty( $params['parent_id'] ) )
+				$params['parent_id'] = ! empty( $params['id'] ) ? intval( $params['id'] ) : 0;
+			
+			$data = array();
+			if ( ! empty( $query_result ) ) {
+				foreach ( $query_result as $post ) {
+					$data = $this->make_data( $post, $data );
+					/**
+					 * Add some extra meta data & filter the data based on 
+					 * 		what part of the relationship we're querying/returning
+					 */
+					if ( $post->post_type == $this->child_type ) {
+						$data[$post->ID] = apply_filters( 'types-relationship-api-final-data', $data[$post->ID], $post );
+						$data[$post->ID][$params['parent']] = get_post( $params['parent_id'] );
+						if ( ! empty( $params['interim'] ) && array_key_exists( $post->ID, $this->interim_posts ) ) {
+							$data[$post->ID][$params['interim']] = $this->interim_posts[$post->ID];
+						}
+					} else if ( $post->post_type == $this->parent_type ) {
+						$data[$post->ID] = apply_filters( 'types-relationship-api-parent-data', $data[$post->ID], $post );
+					} else if ( $post->post_type == $this->interim_type ) {
+						$data[$post->ID] = apply_filters( 'types-relationship-api-interim-data', $data[$post->ID], $post );
+					}
+				}
+			}
+			
+			if ( $respond ) {
+				$final =  $this->create_response( $request, $args, $data );
+				return $final;
+			} else {
+				return $data;
+			}
+		}
+	}
+}
--- classes/class-umw-directory-api.php
+++ classes/class-umw-directory-api.php
@@ -17,6 +17,23 @@ if ( ! class_exists( 'UMW_Directory_API' ) ) {
 			 * Set up our shortcode
 			 */
 			add_shortcode( 'umw-directory', array( $this, 'do_shortcode' ) );
+			add_action( 'init', array( $this, '_add_extra_api_post_type_arguments' ), 12 );
+			
+			/**
+			 * These seem to do nothing at all yet.
+			 */
+			add_filter( 'rest_public_meta_keys', array( $this, 'whitelist_advisory_metadata' ) );
+			add_filter( 'rest_api_allowed_public_metadata', array( $this, 'whitelist_advisory_metadata' ) );
+		}
+		
+		function _add_extra_api_post_type_arguments() {
+			global $wp_post_types;
+			if ( ! array_key_exists( 'employee', $wp_post_types ) )
+				return;
+			
+			$wp_post_types['employee']->show_in_rest = true;
+			$wp_post_types['employee']->rest_base = 'employee';
+			$wp_post_types['employee']->rest_controller_class = 'WP_REST_Posts_Controller';
 		}
 		
 		/**
@@ -34,6 +51,73 @@ if ( ! class_exists( 'UMW_Directory_API' ) ) {
 					require_once( plugin_dir_path( __FILE__ ) . '/inc/class-' . strtolower( str_replace( '_', '-', $c ) ) . '.php' );
 				$this->rest_classes[$k] = new $c;
 			}
+			
+			$this->register_rest_fields();
+		}
+		
+		function register_rest_route_employee_by_username() {
+			require_once( plugin_dir_path( __FILE__ ) . 'class-employee-username-rest-posts-controller.php' );
+			
+			$cb_class = new \Employee_Username_REST_Posts_Controller;
+			
+			$rest_args = array(
+				'per_page' => array(
+					'default' => 10,
+					'sanitize_callback' => 'absint',
+				),
+				'orderby' => array(
+					'default' => 'title', 
+					'sanitize_callback' => array( $cb_class, 'valid_orderby' ), 
+				), 
+				'page' => array(
+					'default' => 1,
+					'sanitize_callback' => 'absint',
+				),
+				'parent_id' => array(
+					'default' => 0, 
+					'sanitize_callback' => 'absint', 
+				), 
+				'slug' => array(
+					'default' => false,
+					'sanitize_callback' => 'sanitize_title',
+				)
+			);
+			
+			$root = 'wp';
+			$version = 'v2';
+			
+			register_rest_route( "{$root}/{$version}", $cb_class->get_route() . '/(?P<username>[\w]+)', array(
+				array(
+					'methods'         => \WP_REST_Server::READABLE,
+					'callback'        => array( $cb_class, 'get_item' ),
+					'args'            => $rest_args, 
+		
+					'permission_callback' => array( $cb_class, 'permissions_check' )
+				),
+			) );
+		}
+		
+		/**
+		 * Register the custom field API endpoints
+		 */
+		function register_rest_fields() {
+			register_rest_field( 
+				'employee', 
+				'wpcf-username', 
+				array( 
+					'get_callback'    => array( $this, 'get_types_field' ), 
+					'update_callback' => array( $this, 'update_types_field' ), 
+					'schema'          => null,
+				)
+			);
+		}
+		
+		/**
+		 * Attempt to whitelist some custom fields to be returned with API data
+		 */
+		function whitelist_advisory_metadata( $keys = array() ) {
+			$keys[] = 'wpcf-username';
+			return $keys;
 		}
 		
 		/**
@@ -74,6 +158,8 @@ if ( ! class_exists( 'UMW_Directory_API' ) ) {
 			foreach ( $this->rest_classes as $c ) {
 				$c->register_routes();
 			}
+			
+			$this->register_rest_route_employee_by_username();
 		}
 		
 		/**
@@ -122,6 +208,24 @@ if ( ! class_exists( 'UMW_Directory_API' ) ) {
 				print( '</code></pre>' );
 				return ob_get_clean();
 			}
+		}
+		
+		/**
+		 * Retrieve the value of a custom field
+		 */
+		function get_types_field( $object, $field_name, $request ) {
+			return get_post_meta( $object[ 'id' ], $field_name );
+		}
+		
+		/**
+		 * Update/set the value of a custom field
+		 */
+		function update_types_field( $value, $object, $field_name ) {
+			if ( ! $value || ! is_string( $value ) ) {
+				return;
+			}
+			
+			return update_post_meta( $object->ID, $field_name, strip_tags( $value ) );
 		}
 		
 		/**
